3faf5fd3b5f6ff03fb4497467598d2bb5601b156
edopore <ediij1994@hotmail.com>
2018-03-08 10:54:32 -0500

    Initial commit

---
+++ app/src/main/java/com/edopore/sesio4g2/MainActivity.java
@@ -0,0 +1,121 @@
+package com.edopore.sesio4g2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText eProyecto, eAvance, ePractica;
+    TextView tNotaF;
+    Button Calcular;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("Método", "onClick");
+
+        eProyecto = findViewById(R.id.ePracticas);
+        eAvance = findViewById(R.id.eAvances);
+        ePractica = findViewById(R.id.eFinal);
+        Calcular = findViewById(R.id.bCalcular);
+        tNotaF = findViewById(R.id.tTotal);
+    }
+
+    public void onButtonClick(View view) {
+        Double proy, pract, avan, notaF;
+        proy = Double.parseDouble(eProyecto.getText().toString());
+        avan = Double.parseDouble(eAvance.getText().toString());
+        pract = Double.parseDouble(ePractica.getText().toString());
+
+        notaF = proy * 0.6 + avan * 0.2 + pract * 0.2;
+        tNotaF.setText(String.valueOf(notaF));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Método", "onPause");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 1234 && resultCode == RESULT_OK) {
+            eProyecto.setText(String.valueOf(data.getExtras().getInt("proy")));
+            eAvance.setText(String.valueOf(data.getExtras().getInt("lab")));
+            ePractica.setText(String.valueOf(data.getExtras().getInt("avance")));
+        } else {
+            if (requestCode == 1234 && resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.mConfig) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            intent.putExtra("proy", 60);
+            intent.putExtra("lab", 20);
+            intent.putExtra("avance", 20);
+            startActivityForResult(intent, 1234);
+            //finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Método", "onRestart");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Método", "onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Método", "onResume");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Método", "onStop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Método", "onDestroy");
+
+    }
+
+}
---
+++ app/src/main/java/com/edopore/sesio4g2/SettingsActivity.java
@@ -0,0 +1,45 @@
+package com.edopore.sesio4g2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SettingsActivity extends AppCompatActivity {
+    EditText eProyecto, eAvance, ePractica;
+    Button bGuardar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        eProyecto = findViewById(R.id.ePracticas);
+        eAvance = findViewById(R.id.eAvances);
+        ePractica = findViewById(R.id.eFinal);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            eAvance.setText(String.valueOf(extras.getInt("lab")));
+            eProyecto.setText(String.valueOf(extras.getInt("proy")));
+            ePractica.setText(String.valueOf(extras.getInt("avance")));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        setResult(RESULT_CANCELED);
+        finish();
+        super.onBackPressed();
+    }
+
+    public void onButtonClicked(View view) {
+        Intent intent = new Intent();
+        intent.putExtra("proy",Integer.parseInt(eProyecto.getText().toString()));
+        intent.putExtra("lab",Integer.parseInt(eAvance.getText().toString()));
+        intent.putExtra("avance",Integer.parseInt(ePractica.getText().toString()));
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+}
